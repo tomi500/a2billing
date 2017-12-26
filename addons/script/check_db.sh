e10af9d52bc9e4d69602c2a6cadde8cb817dd17e
nixonch <nixon@lighttele.com>
2017-12-26 09:55:52 +0200

    Some improvements

--- addons/script/check_db.sh
+++ addons/script/check_db.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-if [ -s "/root/.my.cnf" ] then
-    RES=1
+if [ -s "/root/.my.cnf" ]; then
+    RES=5
     PWD=`/bin/cat /root/.my.cnf |/bin/grep 'password ='|/bin/tail -n 1|/bin/awk -F= '{ print $2 }'|/bin/sed 's/[ \t]*//;s/[ \t]*$//'`
-    while(( $RES!=0 )); do
-	/usr/local/mysql/bin/mysqlcheck --all-databases --auto-repair --password=$PWD
-	RES=$?
-	if ( $RES!=0 ) then
-	    /bin/sleep 10
+    while(( $RES>0 )); do
+	((RES--))
+	if [ -S "/var/run/mysqld/mysql.sock" ]; then
+	    /usr/local/mysql/bin/mysqlcheck --all-databases --auto-repair --password=$PWD
+	    RES=$?
+	fi
+	if (( $RES!=0 )); then
+	    /bin/sleep 5
 	fi
     done
 fi
