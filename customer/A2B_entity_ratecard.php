eec3d017ae9ac17a110ac675835e0937429a48a6
nixonch <nixon@lighttele.com>
2019-11-12 10:44:46 +0200

    Improvement for viewing Ratecard

--- customer/A2B_entity_ratecard.php
+++ customer/A2B_entity_ratecard.php
@@ -93,33 +93,33 @@ $HD_Form -> create_toppage ($form_action);
     <table width="80%" border=0 cellspacing=1 cellpadding=3 bgcolor="#000033" align="center">
         <tr>
        <td bgcolor="#EEEEEE" width="100%" valign="top" align="center" class="bb2">
-	  <a href="A2B_entity_ratecard.php?form_action=list&letter="><?php echo gettext("NONE")?></a> -
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=A">A</a> - 
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=B">B</a> -          
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=C">C</a> -       
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=D">D</a> -       
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=E">E</a> - 
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=F">F</a> -       
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=G">G</a> -       
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=H">H</a> -       
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=I">I</a> - 
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=J">J</a> -       
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=K">K</a> -       
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=L">L</a> -       
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=M">M</a> - 
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=N">N</a> -       
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=O">O</a> -       
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=P">P</a> -       
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=Q">Q</a> - 
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=R">R</a> -       
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=S">S</a> -       
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=T">T</a> -       
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=U">U</a> - 
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=V">V</a> -       
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=W">W</a> -       
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=X">X</a> -       
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=Y">Y</a> - 
-              <a href="A2B_entity_ratecard.php?form_action=list&letter=Z">Z</a>      
+	  <a href="A2B_entity_ratecard.php?form_action=list&letter="><?php echo gettext("NONE")?></a> - 
+              <a href="A2B_entity_ratecard.php?form_action=list&letter=A"><?php if ($letter=="A") echo "<font class=\"blink\">A</font>"; else echo "A";?></a> - 
+              <a href="A2B_entity_ratecard.php?form_action=list&letter=B"><?php if ($letter=="B") echo "<font class=\"blink\">B</font>"; else echo "B";?></a> - 
+              <a href="A2B_entity_ratecard.php?form_action=list&letter=C"><?php if ($letter=="C") echo "<font class=\"blink\">C</font>"; else echo "C";?></a> - 
+              <a href="A2B_entity_ratecard.php?form_action=list&letter=D"><?php if ($letter=="D") echo "<font class=\"blink\">D</font>"; else echo "D";?></a> - 
+              <a href="A2B_entity_ratecard.php?form_action=list&letter=E"><?php if ($letter=="E") echo "<font class=\"blink\">E</font>"; else echo "E";?></a> - 
+              <a href="A2B_entity_ratecard.php?form_action=list&letter=F"><?php if ($letter=="F") echo "<font class=\"blink\">F</font>"; else echo "F";?></a> - 
+              <a href="A2B_entity_ratecard.php?form_action=list&letter=G"><?php if ($letter=="G") echo "<font class=\"blink\">G</font>"; else echo "G";?></a> - 
+              <a href="A2B_entity_ratecard.php?form_action=list&letter=H"><?php if ($letter=="H") echo "<font class=\"blink\">H</font>"; else echo "H";?></a> - 
+              <a href="A2B_entity_ratecard.php?form_action=list&letter=I"><?php if ($letter=="I") echo "<font class=\"blink\">I</font>"; else echo "I";?></a> - 
+              <a href="A2B_entity_ratecard.php?form_action=list&letter=J"><?php if ($letter=="J") echo "<font class=\"blink\">J</font>"; else echo "J";?></a> - 
+              <a href="A2B_entity_ratecard.php?form_action=list&letter=K"><?php if ($letter=="K") echo "<font class=\"blink\">K</font>"; else echo "K";?></a> - 
+              <a href="A2B_entity_ratecard.php?form_action=list&letter=L"><?php if ($letter=="L") echo "<font class=\"blink\">L</font>"; else echo "L";?></a> - 
+              <a href="A2B_entity_ratecard.php?form_action=list&letter=M"><?php if ($letter=="M") echo "<font class=\"blink\">M</font>"; else echo "M";?></a> - 
+              <a href="A2B_entity_ratecard.php?form_action=list&letter=N"><?php if ($letter=="N") echo "<font class=\"blink\">N</font>"; else echo "N";?></a> - 
+              <a href="A2B_entity_ratecard.php?form_action=list&letter=O"><?php if ($letter=="O") echo "<font class=\"blink\">O</font>"; else echo "O";?></a> - 
+              <a href="A2B_entity_ratecard.php?form_action=list&letter=P"><?php if ($letter=="P") echo "<font class=\"blink\">P</font>"; else echo "P";?></a> - 
+              <a href="A2B_entity_ratecard.php?form_action=list&letter=Q"><?php if ($letter=="Q") echo "<font class=\"blink\">Q</font>"; else echo "Q";?></a> - 
+              <a href="A2B_entity_ratecard.php?form_action=list&letter=R"><?php if ($letter=="R") echo "<font class=\"blink\">R</font>"; else echo "R";?></a> - 
+              <a href="A2B_entity_ratecard.php?form_action=list&letter=S"><?php if ($letter=="S") echo "<font class=\"blink\">S</font>"; else echo "S";?></a> - 
+              <a href="A2B_entity_ratecard.php?form_action=list&letter=T"><?php if ($letter=="T") echo "<font class=\"blink\">T</font>"; else echo "T";?></a> - 
+              <a href="A2B_entity_ratecard.php?form_action=list&letter=U"><?php if ($letter=="U") echo "<font class=\"blink\">U</font>"; else echo "U";?></a> - 
+              <a href="A2B_entity_ratecard.php?form_action=list&letter=V"><?php if ($letter=="V") echo "<font class=\"blink\">V</font>"; else echo "V";?></a> - 
+              <a href="A2B_entity_ratecard.php?form_action=list&letter=W"><?php if ($letter=="W") echo "<font class=\"blink\">W</font>"; else echo "W";?></a> - 
+              <a href="A2B_entity_ratecard.php?form_action=list&letter=X"><?php if ($letter=="X") echo "<font class=\"blink\">X</font>"; else echo "X";?></a> - 
+              <a href="A2B_entity_ratecard.php?form_action=list&letter=Y"><?php if ($letter=="Y") echo "<font class=\"blink\">Y</font>"; else echo "Y";?></a> - 
+              <a href="A2B_entity_ratecard.php?form_action=list&letter=Z"><?php if ($letter=="Z") echo "<font class=\"blink\">Z</font>"; else echo "Z";?></a> 
        </td>
         </tr>
     </table>
